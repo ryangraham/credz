1b7f64d72386cb1bbcf43f2bf2273f2c664ae801
Ryan Graham <rgraham@blvd.co>
2020-05-14 00:36:26 -0700

    tidy

--- src/unescape.h
+++ src/unescape.h
@@ -10,16 +10,18 @@
  * https://en.wikipedia.org/wiki/Numeric_character_reference
  */
 inline std::string unescape(const std::string &input) {
+  const int BASE_HEX = 16;
   std::string output;
   boost::regex re("&#x([a-zA-Z0-9][a-zA-Z0-9]);");
-  const int subs[] = {-1, 1};
+  const std::vector<int> submatches = {-1, 1};
   boost::sregex_token_iterator end;
 
-  boost::sregex_token_iterator token(input.begin(), input.end(), re, subs);
+  boost::sregex_token_iterator token(input.begin(), input.end(), re,
+                                     submatches);
 
   for (bool is_ncr = false; token != end; ++token, is_ncr = !is_ncr) {
     if (is_ncr) {
-      unsigned int d = std::stoul("0x" + *token, nullptr, 16);
+      unsigned int d = std::stoul("0x" + *token, nullptr, BASE_HEX);
       output += static_cast<char>(d);
     } else
       output += *token;
