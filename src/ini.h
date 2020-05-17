792276d2ab469f965b1216954f1478079f7c6f1d
Ryan Graham <rgraham@blvd.co>
2020-05-16 19:20:52 -0700

    convert ini from just a header

--- src/ini.h
+++ src/ini.h
@@ -1,65 +1,20 @@
-#include <boost/filesystem.hpp>
-#include <boost/filesystem/fstream.hpp>
-#include <boost/property_tree/ini_parser.hpp>
+#pragma once
 #include <boost/property_tree/ptree.hpp>
 #include <string>
 
 #include "aws.h"
 #include "path.h"
 
-#define AWS_CRED_DIR "~/.aws/"
-#define AWS_CRED_FILE "credentials"
-#define AWS_CRED_FULL_PATH AWS_CRED_DIR AWS_CRED_FILE
-
 namespace ini {
 
 bool profile_exists(const boost::property_tree::ptree &root,
-                    const std::string &profile_name) {
-  return root.count(profile_name) != 0;
-}
+                    const std::string &profile_name);
 
 aws::profile get_profile(const boost::property_tree::ptree &root,
-                         const std::string &profile_name) {
-  aws::profile profile;
-  profile.name = profile_name;
-  profile.aws_access_key_id =
-      root.get<std::string>(profile_name + ".aws_access_key_id");
-  profile.aws_secret_access_key =
-      root.get<std::string>(profile_name + ".aws_secret_access_key");
-  profile.aws_session_token =
-      root.get<std::string>(profile_name + ".aws_session_token");
-  return profile;
-}
+                         const std::string &profile_name);
 
-void put_profile(boost::property_tree::ptree root,
-                 const aws::profile &profile) {
-  root.put(profile.name + ".aws_access_key_id", profile.aws_access_key_id);
-  root.put(profile.name + ".aws_secret_access_key",
-           profile.aws_secret_access_key);
-  root.put(profile.name + ".aws_session_token", profile.aws_session_token);
+void put_profile(boost::property_tree::ptree root, const aws::profile &profile);
 
-  std::string full_path = AWS_CRED_FULL_PATH;
-  path::expand(full_path);
-
-  std::string dir_path = AWS_CRED_DIR;
-  path::expand(dir_path);
-
-  boost::filesystem::create_directories(dir_path);
-
-  write_ini(full_path, root);
-}
-
-boost::property_tree::ptree load_file() {
-  boost::property_tree::ptree root;
-
-  std::string full_path = AWS_CRED_FULL_PATH;
-  path::expand(full_path);
-
-  if (!boost::filesystem::exists(full_path)) return root;
-
-  boost::property_tree::ini_parser::read_ini(full_path, root);
-
-  return root;
-}
+boost::property_tree::ptree load_file();
 
 }  // namespace ini
