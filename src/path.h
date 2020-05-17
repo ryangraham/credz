d78183b90f029eb57de13cdb8d4a8aca8d452fa0
Ryan Graham <rgraham@blvd.co>
2020-05-17 13:24:07 -0700

    tests

--- src/path.h
+++ src/path.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <pwd.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <wordexp.h>
 
 #include <string>
+
+#include "doctest/doctest.h"
 
 namespace path {
 
@@ -14,4 +19,22 @@ inline void expand(std::string &path) {
   wordfree(&exp_result);
 }
 
+inline std::string get_home_dir() {
+  char *home_dir;
+
+  home_dir = getenv("HOME");
+
+  if (home_dir == nullptr) home_dir = getpwuid(getuid())->pw_dir;
+
+  return std::string(home_dir);
+}
+
 }  // namespace path
+
+TEST_CASE("testing path expansion") {
+  std::string home_dir = path::get_home_dir();
+  std::string expansion = "~";
+  path::expand(expansion);
+
+  CHECK(expansion == home_dir);
+}
