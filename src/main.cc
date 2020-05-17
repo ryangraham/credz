4bd65ea4e35990657295ebbfa26617a13991c35b
Ryan Graham <rgraham@blvd.co>
2020-05-17 13:42:53 -0700

    tests

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,3 @@
-#include <doctest/doctest.h>
-
 #include <string>
 
 #include "aws.h"
@@ -34,16 +32,4 @@ int main(int argc, char* argv[]) {
   curl_global_cleanup();
 
   return EXIT_SUCCESS;
-}
-
-int factorial(const int number) {
-  return number < 1 ? 1 : number <= 1 ? number : factorial(number - 1) * number;
-}
-
-TEST_CASE("testing the factorial function") {
-  CHECK(factorial(0) == 1);
-  CHECK(factorial(1) == 1);
-  CHECK(factorial(2) == 2);
-  CHECK(factorial(3) == 6);
-  CHECK(factorial(10) == 3628800);
 }
